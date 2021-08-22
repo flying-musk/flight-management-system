9cea959074b71650f3944d2e43785b7a91a123e5
Flying Musk <musk.flying@gmail.com>
2021-08-22 17:37:23 +0800

    [flight-management-system] Init project and set up eslint rules.

---
+++ src/components/FlightManagementSystem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="f-outer">Flight Management System</div>
+</template>
+
+<script>
+export default {
+  name: 'flight-management-system',
+};
+</script>
+
+<style scoped lang="scss">
+.f-outer {
+  border: solid 1px #cccccc;
+}
+</style>
